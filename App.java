49173d5c8a29fe714472daacb273562b5c26cb14
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-08 08:20:03 +0900

    FIzzBuzz

--- App.java
+++ App.java
@@ -1,5 +1,15 @@
 public class App {
   public static void main(String[] args) {
-    System.out.println("hello,world!");
+    for (int n = 1; n <= 20; n++){
+     if (n % 3 == 0 && n % 5 == 0){
+      System.out.println("FizzBuzz");
+      } else if (n % 3 == 0){
+        System.out.println("Fizz");
+      } else if (n % 5 == 0){
+        System.out.println("Bizz"); 
+      } else {
+        System.out.println(n);
       }
+    }
   }
+}
