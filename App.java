cb6b38f3f0f8df2b7b78fe2004a42360cdd23334
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-08 08:29:23 +0900

    変数と型と演算

--- App.java
+++ App.java
@@ -1,15 +1,12 @@
 public class App {
-  public static void main(String[] args) {
-    for (int n = 1; n <= 20; n++){
-     if (n % 3 == 0 && n % 5 == 0){
-      System.out.println("FizzBuzz");
-      } else if (n % 3 == 0){
-        System.out.println("Fizz");
-      } else if (n % 5 == 0){
-        System.out.println("Bizz"); 
-      } else {
-        System.out.println(n);
-      }
-    }
+  public static void main(String[] args){
+    //変数宣言
+    int price = 9260;
+    int discount_rate = 33;
+    
+   //宣言した変数を利用して表示
+   System.out.println("商品の金額は" + price + "円です");
+   System.out.println("消費税率は"+ discount_rate + "%です");
+   System.out.println("税込み金額は" + (price * discount_rate * 0.01) + "円です");
   }
 }
