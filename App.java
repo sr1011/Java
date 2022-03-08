b014e57589b1adb08dbb851da73893f0750cc39e
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-08 13:50:43 +0900

    7の階乗

--- App.java
+++ App.java
@@ -1,9 +1,9 @@
 public class App {
   public static void main(String[] args){
-    int x = 2;
-    for (int i = 1;i <= 8; i++){
+    int x = 1;
+    for (int i = 7;i > 0; i--){
       x *= i;
-      System.out.println(x);
     }
+    System.out.println(x);
   }
 }
