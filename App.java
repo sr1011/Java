906c32dd0458d79954891f2a57dcb7aa8bfcf7f9
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-14 17:56:09 +0900

    commit

--- App.java
+++ App.java
@@ -1,10 +1,10 @@
 public class App {
   public static void main(String[] args){
     for(int i=0; i<5; i++){
-      for(int j=0; j<i; j++){
+      for(int j=0; j<5-(i+1); j++){
         System.out.print(" ");
       }
-      for(int k=0; k<(5-i)*2-1; k++){
+      for(int k=0; k<(i+1)*2-1; k++){
         System.out.print("*");
       }
       System.out.print("\n");
