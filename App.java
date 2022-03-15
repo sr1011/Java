4f35e9ccba0264b05224b460331e42ff534ea330
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-15 14:05:17 +0900

    定数

--- App.java
+++ App.java
@@ -1,8 +1,12 @@
 public class App {
   public static void main(String[] args){
-    int age = 20;
-    System.out.println("私の年齢は" + age);
-    age = 30;
-    System.out.println("いや、本当の年齢は" + age);
+    final double PI  = 3.14;
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * PI);
+    System.out.println("パイの半径を倍にします");
+    pie = 10;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * PI);
   }
 }
