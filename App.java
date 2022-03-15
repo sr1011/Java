353334c3d672f06332b1fad85c5841b87d059806
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-15 13:41:56 +0900

    変数宣言

--- App.java
+++ App.java
@@ -1,13 +1,7 @@
 public class App {
   public static void main(String[] args){
-    for(int i=0; i<5; i++){
-      for(int j=0; j<5-(i+1); j++){
-        System.out.print(" ");
-      }
-      for(int k=0; k<(i+1)*2-1; k++){
-        System.out.print("*");
-      }
-      System.out.print("\n");
-    }
+    int age;
+    age = 30;
+    System.out.println(age);
   }
 }
