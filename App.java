534ce96b91f10bdb66f2dff7cea47b5695321df5
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-15 13:53:20 +0900

    boolean

--- App.java
+++ App.java
@@ -1,9 +1,9 @@
 public class App {
   public static void main(String[] args){
-    float weight; weight = 67.5F;
-    double height; height = 171.2;
+    boolean isError; isError = true;
+    boolean result; result = false;
 
-    System.out.println(weight);
-    System.out.println(height);
+    System.out.println(isError);
+    System.out.println(result);
   }
 }
