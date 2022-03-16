98350892dbd53c18a9815a7e2ddb5dbdb81149f4
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-16 15:57:54 +0900

    エスケープシーケンス

--- App.java
+++ App.java
@@ -1,9 +1,5 @@
 public class App {
   public static void main(String[] args){
-    int a,b;
-    a = 20;
-    b = a + 5;
-    System.out.println(a);
-    System.out.println(b);
+    System.out.println("私の好きな記号は二重引用符（\"）です。¥1200");
   }
 }
