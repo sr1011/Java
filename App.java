a1c3abe75969f4bc59703c249e1c5331dd93ff8e
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-16 15:50:17 +0900

    計算文

--- App.java
+++ App.java
@@ -1,7 +1,9 @@
 public class App {
   public static void main(String[] args){
-    int a = 3, b = 5;
-    System.out.print("縦幅" + a + "横幅" + b + "の長方形の面積は、");
-    System.out.println(a * b);
+    int a,b;
+    a = 20;
+    b = a + 5;
+    System.out.println(a);
+    System.out.println(b);
   }
 }
