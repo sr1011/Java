f3a56922eea0e58300414627b309cf3a60b75961
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-17 21:56:42 +0900

    命令実行文

--- App.java
+++ App.java
@@ -1,8 +1,8 @@
 public class App {
   public static void main(String[] args){
-    double d = 8.5 / 2;
-    long l = 5 + 2L;
-    System.out.println(d);
-    System.out.println(l);
+    String name = "すがわら";
+    String message;
+    message = name + "さん、こんにちは";
+    System.out.println(message);
   }
 }
