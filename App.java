a3951700222cdbc28769b75471295cc465871a0a
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-18 17:26:54 +0900

    練習2-3

--- App.java
+++ App.java
@@ -1,8 +1,15 @@
 public class App {
   public static void main(String[] args){
-    int x = 5;
-    int y = 10;
-    String ans = "x＋yは" + (x + y);
-    System.out.println(ans);
+    System.out.println("ようこそ占いの館へ");
+    System.out.println("あなたの名前を入力してください。");
+    String name = new java.util.Scanner(System.in) .nextLine();
+    System.out.println("あなたの年齢を入力してください。");
+    String ageString = new java.util.Scanner(System.in) .nextLine();
+    int age = Integer.parseInt(ageString);
+    int fortune = new java.util.Random() .nextInt(3);
+    fortune++;
+    System.out.println("占いの結果がでました。");
+    System.out.println(age + "歳の" + name + "さん、あなたの運勢番号は" + fortune + "です");
+    System.out.println("1:大吉　2:吉　3:中吉　4:凶");
   }
 }
