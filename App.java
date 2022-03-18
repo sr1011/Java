b45b386c471cd48b3eb4b2042e94cab464231d82
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-18 16:43:51 +0900

    System.out.print

--- App.java
+++ App.java
@@ -1,8 +1,8 @@
 public class App {
   public static void main(String[] args){
     String name = "すがわら";
-    String message;
-    message = name + "さん、こんにちは";
-    System.out.println(message);
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
   }
 }
