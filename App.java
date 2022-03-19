8ef1e6cc2d5a6d15658848220ca2846099d30906
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-19 14:41:36 +0900

    while文

--- App.java
+++ App.java
@@ -1,15 +1,19 @@
 public class App {
   public static void main(String[] args){
     System.out.println("あなたの運勢を占います");
-    int fortune = new java.util.Random() .nextInt(4) + 1;
-    if (fortune == 1) {
-      System.out.println("大吉");
-    } else if (fortune == 2){
-      System.out.println("吉");
-    } else if (fortune == 3){
-      System.out.println("中吉");
-    } else if (fortune == 4){
-      System.out.println("凶");
+    int fortune = new java.util.Random() .nextInt(5) + 1;
+    switch (fortune){
+      case 1:
+      case 2:
+       System.out.println("いいね");
+       break;
+      case 3:
+       System.out.println("普通です");
+       break;
+      case 4:
+      case 5:
+      System.out.println("うーん。。。");
+      break;
     }
   }
 }
