bdf906a1bfefc859e23c5d0a43785b20e1fc5045
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-19 14:34:19 +0900

    if elseで占い

--- App.java
+++ App.java
@@ -1,9 +1,15 @@
 public class App {
   public static void main(String[] args){
-    boolean doorClose = true;
-    whili (doorClose == true){
-    System.out.println("ノックする");
-    System.out.println("1分待つ");
+    System.out.println("あなたの運勢を占います");
+    int fortune = new java.util.Random() .nextInt(4) + 1;
+    if (fortune == 1) {
+      System.out.println("大吉");
+    } else if (fortune == 2){
+      System.out.println("吉");
+    } else if (fortune == 3){
+      System.out.println("中吉");
+    } else if (fortune == 4){
+      System.out.println("凶");
     }
   }
 }
