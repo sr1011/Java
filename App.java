5920c4d7496a173f2587fcad31cc751e04f10b7e
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-19 13:55:14 +0900

    天気をJavaで表す

--- App.java
+++ App.java
@@ -1,15 +1,11 @@
 public class App {
   public static void main(String[] args){
-    System.out.println("ようこそ占いの館へ");
-    System.out.println("あなたの名前を入力してください。");
-    String name = new java.util.Scanner(System.in) .nextLine();
-    System.out.println("あなたの年齢を入力してください。");
-    String ageString = new java.util.Scanner(System.in) .nextLine();
-    int age = Integer.parseInt(ageString);
-    int fortune = new java.util.Random() .nextInt(3);
-    fortune++;
-    System.out.println("占いの結果がでました。");
-    System.out.println(age + "歳の" + name + "さん、あなたの運勢番号は" + fortune + "です");
-    System.out.println("1:大吉　2:吉　3:中吉　4:凶");
+    boolean tenki = true;
+    if (tenki == true){
+    System.out.println("選択をします。");
+    System.out.println("散歩に行きます。");
+    } else {
+      System.out.println("DVDを見ます。");
+    }
   }
 }
