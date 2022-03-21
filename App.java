22c5a96b93be70f1df43683380f933472283613e
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-21 15:04:24 +0900

    練習3-6

--- App.java
+++ App.java
@@ -1,15 +1,17 @@
 public class App {
   public static void main(String[] args){
-    System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更");
-    int select = new java.util.Scanner(System.in) .nextInt();
-    if (select == 1){
-      System.out.println("検索します");
-    } else if (select == 2){
-      System.out.println("登録します");
-    } else if (select == 3){
-      System.out.println("削除します");
-    } else if (select == 4){
-      System.out.println("変更します");
+    System.out.print("数あてゲーム");
+    int ans = new java.util.Random() .nextInt(9);
+    for (int i = 0; i < 5; i++){
+      System.out.println("0~9の数字を入力してください");
+      int num = new java.util.Scanner(System.in) .nextInt();
+      if (ans == num){
+        System.out.println("アタリ!!");
+        break;
+      } else {
+        System.out.println("違います");
+      }
     }
+    System.out.println("ゲームを終了します");
   }
 }
