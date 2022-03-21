299ff5309ac1519df97e69e27d30606886e6e5af
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-21 14:29:18 +0900

    ネスト

--- App.java
+++ App.java
@@ -1,7 +1,11 @@
 public class App {
   public static void main(String[] args){
-    for (int i = 1; i < 3; i++) {
-      System.out.println("現在" + i + "週目");
+    for (int i = 1; i < 10; i++) {
+      for (int j = 1; j < 10; j++){
+        System.out.print(i * j);
+        System.out.print(" ");
+      }
+      System.out.println(" ");
     }
   }
 }
