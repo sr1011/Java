d771fad77a3de3eb6ee847f3adfee96f722c2abe
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-23 15:22:58 +0900

    拡張for文

--- App.java
+++ App.java
@@ -1,14 +1,8 @@
 public class App {
   public static void main(String[] args){
-    int[] seq = new int [10];
-
-    for (int i = 0; i < seq.length; i++){
-      seq[i] = new java.util.Random() .nextInt(4);
-      }
-
-      for (int i = 0; i < seq.length; i++) {
-        char[] base = {'A', 'T', 'G', 'C'};
-        System.out.println(base[seq[i]] + " ");
-      }
+    int[] scores = {20, 30, 40, 50, 80};
+    for (int value : scores) {
+      System.out.println(value);
     }
+  }
 }
