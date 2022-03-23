1fa4e4be2a05cfbb43c9cba97c7c90de5a601f7d
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-23 15:42:23 +0900

    nullの利用(エラー)

--- App.java
+++ App.java
@@ -1,9 +1,7 @@
 public class App {
   public static void main(String[] args){
-    int[] arrayA = {1, 2, 3};
-    int[] arrayB;
-    arrayB = arrayA;
-    arrayB[0] = 100;
-    System.out.println(arrayA[0]);
+    int[] array = {1, 2, 3};
+    array = null;
+    array[0] = 10;
   }
 }
