6c89ca17e4556d4e4650baf9c9919f35543194e0
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-24 15:37:21 +0900

    練習4-4(別解)

--- App.java
+++ App.java
@@ -3,8 +3,8 @@ public class App {
     int[] numbers = {3, 4, 9};
     System.out.println("1桁の数字を入力してください");
     int input = new java.util.Scanner(System.in) .nextInt();
-    for (int i = 0; i < numbers.length; i++){
-      if (numbers[i] == input){
+    for (int n:numbers) {
+      if (n == input){
         System.out.println("アタリ！");
       }
     }
