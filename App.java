c8827d7d531180d83276846c9f275536a933b1b8
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-24 15:11:31 +0900

    練習4-1

--- App.java
+++ App.java
@@ -1,7 +1,8 @@
 public class App {
   public static void main(String[] args){
-    int[][] scores = {{40, 50, 60}, {80, 60, 70}};
-    System.out.println(scores.length);
-    System.out.println(scores[0].length);
+    int[] points = new int[4];
+    double[] weigths = new double[5];
+    boolean[] ansewrs = new boolean[3];
+    String[] names = new String[3];
   }
 }
