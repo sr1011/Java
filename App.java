b6f3e6d9a04b8a08a9ae347958b332e88a8f2dd7
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-24 15:36:21 +0900

    練習4-4

--- App.java
+++ App.java
@@ -1,11 +1,12 @@
 public class App {
   public static void main(String[] args){
-    int[] moneyList = {121902, 8302, 55100};
-    for (int i = 0; i < moneyList.length; i++){
-      System.out.println(moneyList[i]);
-    }
-    for (int list:moneyList){
-      System.out.println(list);
+    int[] numbers = {3, 4, 9};
+    System.out.println("1桁の数字を入力してください");
+    int input = new java.util.Scanner(System.in) .nextInt();
+    for (int i = 0; i < numbers.length; i++){
+      if (numbers[i] == input){
+        System.out.println("アタリ！");
+      }
     }
   }
 }
