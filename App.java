0a1d5eb3dc596620fc0f097b000ae0365041af6d
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-24 15:20:50 +0900

    練習4-2

--- App.java
+++ App.java
@@ -1,8 +1,11 @@
 public class App {
   public static void main(String[] args){
-    int[] points = new int[4];
-    double[] weigths = new double[5];
-    boolean[] ansewrs = new boolean[3];
-    String[] names = new String[3];
+    int[] moneyList = {121902, 8302, 55100};
+    for (int i = 0; i < moneyList.length; i++){
+      System.out.println(moneyList[i]);
+    }
+    for (int list:moneyList){
+      System.out.println(list);
+    }
   }
 }
