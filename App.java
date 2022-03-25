afb7e0649e81666e5bd18e91e4da7e9c4da8549e
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-25 15:38:08 +0900

    引数

--- App.java
+++ App.java
@@ -1,14 +1,12 @@
 public class App {
-  public static void methodA(){
-    System.out.println("methodA");
-    methodB();
-  }
-
-  public static void methodB(){
-    System.out.println("methodB");
-  }
-
   public static void main(String[] args){
-    methodA();
+    System.out.println("メソッドを呼び出します");
+    hello("湊");
+    hello("朝香");
+    hello("菅原");
+    System.out.println("メソッドの呼び出しが終わりました");
+  }
+  public static void hello(String name) {
+    System.out.println(name + "さん、こんにちは");
   }
 }
