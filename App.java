0130f427aa0f769fbd5268751bbb572bc60e92c0
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-25 16:08:06 +0900

    戻り値をそのまま使用

--- App.java
+++ App.java
@@ -1,7 +1,6 @@
 public class App {
   public static void main(String[] args){
-    int ans = add(100, 10);
-    System.out.println("100 + 10 = " + ans);
+    System.out.println(add(add(10, 20), add(30, 40)));
   }
   public static int add (int x, int y) {
     int ans = x + y;
