4c2e7336020f17d43e9c68f899a9aa1aa0d083c6
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-26 14:55:22 +0900

    オーバーロード（因数の数が違う場合）

--- App.java
+++ App.java
@@ -1,18 +1,13 @@
 public class App {
   public static void main(String[] args){
-    System.out.println(add(10, 20));
-    System.out.println(add(3.5, 2.7));
-    System.out.println(add("Hello", "World"));
+    System.out.println("10 + 20 = " + add(10, 20));
+    System.out.println("10 + 20 + 30 = " + add(10, 20, 30));
   }
   public static int add (int x, int y) {
     return x + y;
   }
 
-  public static double add (double x, double y) {
-    return x + y;
-  }
-
-  public static String add (String x, String y) {
-    return x + y;
+  public static int add (int x, int y, int z) {
+    return x + y + z;
   }
 }
