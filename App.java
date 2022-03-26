4afc5d5bd58c0d28325d04de3cc59aa689d475fb
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-26 15:01:08 +0900

    因数の配列

--- App.java
+++ App.java
@@ -1,13 +1,11 @@
 public class App {
   public static void main(String[] args){
-    System.out.println("10 + 20 = " + add(10, 20));
-    System.out.println("10 + 20 + 30 = " + add(10, 20, 30));
+    int[] array = {1, 2, 3};
+    printArray(array);
   }
-  public static int add (int x, int y) {
-    return x + y;
-  }
-
-  public static int add (int x, int y, int z) {
-    return x + y + z;
+  public static void printArray (int[] array) {
+    for (int element : array) {
+      System.out.println(element);
+    }
   }
 }
