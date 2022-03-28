7c0b5894f56dc9f51018e28e846b983b4721a66c
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-28 15:22:38 +0900

    戻り値に配列

--- App.java
+++ App.java
@@ -1,14 +1,15 @@
 public class App {
   public static void main(String[] args){
-    int[] array = {1, 2, 3};
-    printArray(array);
+    int[] array = makeArray(3);
     for (int i : array) {
       System.out.println(i);
     }
   }
-  public static void printArray (int[] array) {
-    for (int i = 0; i < array.length; i++) {
-      array[i]++;
+  public static int[] makeArray (int size) {
+    int[] newArray = new int[size];
+    for (int i = 0; i < newArray.length; i++) {
+      newArray[i] = i;
     }
+    return newArray;
   }
 }
