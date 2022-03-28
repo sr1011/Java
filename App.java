081fcc4a7edc5a22b7cf957fc2896476e85805d9
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-28 15:14:25 +0900

    参照渡し

--- App.java
+++ App.java
@@ -2,11 +2,13 @@ public class App {
   public static void main(String[] args){
     int[] array = {1, 2, 3};
     printArray(array);
+    for (int i : array) {
+      System.out.println(i);
+    }
   }
   public static void printArray (int[] array) {
-    array[0] = 100;
-    for (int element : array) {
-      System.out.println(element);
+    for (int i = 0; i < array.length; i++) {
+      array[i]++;
     }
   }
 }
