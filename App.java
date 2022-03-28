96768374a319fe4a785936157f51366f2a67dca2
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-28 16:19:58 +0900

    練習5-3

--- App.java
+++ App.java
@@ -3,12 +3,18 @@ public class App {
     String title = "test";
     String address = "アドレス";
     String text = "testtext";
-    email(title, address, text);
+    email(address, text);
   }
 
   public static void email(String title, String address, String text) {
     System.out.println(address + "に、以下のメールを送信しました");
     System.out.println("件名：" + title);
+    System.out.println("本文：" + text);
+  }
+
+  public static void email(String address, String text) {
+    System.out.println(address + "に、以下のメールを送信しました");
+    System.out.println("件名：無題");
     System.out.println("本文：" + text);
   }
 }
