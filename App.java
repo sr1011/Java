c631a2eb6e89528d28bae749be97d9c6b96972db
sr1011 <ryo.s.1011.football@gmail.com>
2022-03-31 14:12:02 +0900

    API

--- App.java
+++ App.java
@@ -1,11 +1,9 @@
-package calcapp.main;
-import calcapp.logics.*;
-
 public class App {
   public static void main(String[] args){
-    int a = 10; int b = 2;
-    int total = CalcLogic.tasu(a, b);
-    int delta = CalcLogic.hiku(a, b);
-    System.out.println("足すと" + total + "、引くと" + delta);
+    int[] heights = {172, 149, 152, 191,155};
+    java.util.Arrays.sort(heights);
+    for (int h : heights) {
+      System.out.println(h);
+  }
   }
 }
