431ceb3028dea5d96fffe1365942680308351cb1
sr1011 <ryo.s.1011.football@gmail.com>
2022-04-03 16:24:34 +0900

    冒険の始まり

--- App.java
+++ App.java
@@ -2,13 +2,25 @@ public class App {
   public static void main(String[] args){
     // 勇者を生成
     Hero h = new Hero();
-    // フィールドに初期化をセット
+    //フィールドに初期化をセット
     h.name = "ミナト";
     h.hp = 100;
-    System.out.println("勇者" + h.name + "を生み出しました！");
-    h.sit(5);
+
+    //おばけキノコm1を生成
+    Matango m1 = new Matango();
+    //フィールドに初期化をセット
+    m1.hp = 50;
+    m1.suffix = 'A';
+
+    //おばけキノコm2を生成
+    Matango m2 = new Matango();
+    //フィールドに初期化をセット
+    m1.hp = 48;
+    m1.suffix = 'B';
+
     h.slip();
-    h.sit(25);
+    m1.run();
+    m2.run();
     h.run();
   }
 }
