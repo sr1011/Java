9b7b75badfbb267ddf2d275995793846e47098e4
sr1011 <ryo.s.1011.football@gmail.com>
2022-04-05 20:28:43 +0900

    Scanner

---
+++ Learn.java
@@ -0,0 +1,7 @@
+public class Learn {
+  public static void main(String[] args){
+    System.out.println("名前を入力してください");
+    String s = new java.util.Scanner(System.in) .nextLine();
+    System.out.println("あなたの名前は"  + s + "です");
+  }
+}
