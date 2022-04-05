e1d08016aaf6255f781b21eb1aa1005131b6d25b
sr1011 <ryo.s.1011.football@gmail.com>
2022-04-05 21:46:36 +0900

    練習問題３−１

--- Learn.java
+++ Learn.java
@@ -1,7 +1,14 @@
 public class Learn {
   public static void main(String[] args){
-    System.out.println("名前を入力してください");
-    String s = new java.util.Scanner(System.in) .nextLine();
-    System.out.println("あなたの名前は"  + s + "です");
+    System.out.println("1つ目の整数を入力してください");
+      int a1 = new java.util.Scanner(System.in) .nextInt();
+      System.out.println("2つ目の整数を入力してください");
+      int a2 = new java.util.Scanner(System.in) .nextInt();
+
+      if (a1 > a2){
+        System.out.println("大きい数は" + a1 + "です");
+      } else if(a1 < a2) {
+        System.out.println("大きい数は" + a2 + "です");
+      }
   }
 }
